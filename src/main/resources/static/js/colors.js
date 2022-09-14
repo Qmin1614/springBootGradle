b9ac29a6ff5a027ae24bcfb3e6c06e30db39e2a3
pkm1614 <pkm1614@gmail.com>
2022-09-14 17:54:00 +0900

    2022-09-14 : JavaScript study-jQuery

--- src/main/resources/static/js/colors.js
+++ src/main/resources/static/js/colors.js
@@ -1,5 +1,5 @@
 let Links = {
-    setColor:function(color){
+    setColor:function(color) {
 /*        JavaScript
         let alist = document.querySelectorAll('a');
         var i = 0;
@@ -12,18 +12,18 @@ let Links = {
 }
 
 let Body = {
-    setColor:function (color){
+    setColor:function (color) {
         // document.querySelector('body').style.color = color;
         $('body').css('color', color);
     },
-    setBackgroundColor:function (color){
+    setBackgroundColor:function (color) {
         //document.querySelector('body').style.backgroundColor = color;
         $('body').css('backgroundColor', color);
     }
 }
 
 function nightDayHandler(self){
-    if(self.value === 'night'){
+    if(self.value === 'night') {
         Body.setBackgroundColor('black');
         Body.setColor('white');
         self.value = 'day';
